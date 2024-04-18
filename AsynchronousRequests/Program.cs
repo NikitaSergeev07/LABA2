bbd3fd41d43edbf7490bde17cd7afde2bab86d3f
Nicolas <nikitas200417g@yandex.ru>
2024-04-19 00:33:16 +0300

    OOP_Laba2
    OOP_Laba2

---
+++ AsynchronousRequests/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net.Http;
+using System.IO;
+using System.Net.Http.Json;
+using System.Reflection.Metadata;
+using System.Diagnostics;
+
+class Program
+{
+    static HttpClient httpClient = new HttpClient();
+    
+    static async Task Main()
+    {
+        
+        httpClient.DefaultRequestHeaders.Add("User-Agent", "MyApp");
+
+        try
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            var result = await GetResponse(httpClient, "https://api.hh.ru/vacancies");
+            Console.WriteLine(result);
+            stopwatch.Stop();
+            Console.WriteLine($"Время выполнения: {stopwatch.ElapsedMilliseconds} мс");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        
+
+    }
+    static async Task<object> GetResponse(HttpClient httpClient, string url)
+    {
+        using HttpResponseMessage response = await httpClient.GetAsync(url);
+        var content = await response.Content.ReadFromJsonAsync<object>();
+        if (response.IsSuccessStatusCode)
+        {
+            return content;
+        }
+        else
+        {
+            throw new ArgumentException("Not valid response");
+        }
+    }
+
+
+}
---
+++ SynchronousRequests/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net.Http;
+using System.IO;
+using System.Net.Http.Json;
+using System.Reflection.Metadata;
+using System.Diagnostics;
+
+class Program
+{
+    static HttpClient httpClient = new HttpClient();
+
+    static void Main()
+    {
+        httpClient.DefaultRequestHeaders.Add("User-Agent", "MyApp1");
+
+        try
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            GetResponse(httpClient, "https://api.hh.ru/vacancies");
+            stopwatch.Stop();
+            Console.WriteLine($"Время выполнения: {stopwatch.ElapsedMilliseconds} мс");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+    }
+    static void GetResponse(HttpClient httpClient, string url)
+    {
+        HttpResponseMessage response = httpClient.GetAsync(url).Result;
+        var content = response.Content.ReadAsStringAsync().Result;
+        if (response.IsSuccessStatusCode)
+        {
+            Console.WriteLine(content);
+        }
+        else
+        {
+            throw new ArgumentException("Not valid response");
+        }
+    }
+
+
+}
